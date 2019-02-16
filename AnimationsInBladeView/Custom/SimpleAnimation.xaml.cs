9cc2f09640dc2e42159af851ff4b7b362e50b2ef
Dan Meier <Dan.Meier@FactorySoftworks.com>
2019-02-16 13:40:08 -0800

    Add project files.

---
+++ AnimationsInBladeView/Custom/SimpleAnimation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace AnimationsInBladeView.Custom
+{
+    public sealed partial class SimpleAnimation : UserControl
+    {
+        public SimpleAnimation()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AnimateButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            AnimateButton.Visibility = Visibility.Collapsed;
+            ElementStack.Visibility  = Visibility.Visible;
+        }
+
+        private void CancelButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            AnimateButton.Visibility = Visibility.Visible;
+            ElementStack.Visibility  = Visibility.Collapsed;
+        }
+    }
+}
---
+++ AnimationsInBladeView/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace AnimationsInBladeView
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ImplicitAnimationsBladeView.Visibility = Visibility.Visible;
+            ImplicitAnimationsBladeItem.IsOpen     = true;
+        }
+    }
+}
